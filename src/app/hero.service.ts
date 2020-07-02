a2948f1bcb7d8698912a85a89b7717d01d7135bf
saad2379 <saad2379@gmail.com>
2020-07-02 20:21:42 +0100

    added mock Http calls, need to fix css for hero-detail

---
+++ src/app/hero.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {Hero} from './heroes/hero';
+import {HEROES} from "./heroes/mock-heroes";
+import {Observable, of} from "rxjs";
+import {MessageService} from "./message.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, map, tap} from "rxjs/operators";
+
+/**
+ * Service to get hero data
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  //injecting message service in this service which injects it into the hero component
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
+
+  private heroesUrl = 'api/heroes';
+
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_=> this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return of(HEROES.find(hero => hero.id === id))
+      .pipe(
+        tap(_=> this.log(`fetched hero ${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+}
